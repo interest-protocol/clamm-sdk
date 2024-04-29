c19c32f4450171faf834e10acfacf381ec04aa7c
Jose Cerqueira <josecerqueira@protonmail.com>
2024-04-29 19:04:01 +0100

    :test_tube: test: remove one coin fns

---
+++ src/scripts/stable-pool/remove-liquidity-one-coin.script.ts
@@ -0,0 +1,41 @@
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+import {
+  CLAMM,
+  executeTx,
+  getCoinOfValue,
+  keypair,
+  log,
+  STABLE_POOL_USDC_USDT_OBJECT_ID,
+} from '../utils.script';
+
+(async () => {
+  try {
+    const pool = await CLAMM.getPool(STABLE_POOL_USDC_USDT_OBJECT_ID);
+
+    const initTxb = new TransactionBlock();
+
+    const lpCoin = await getCoinOfValue(initTxb, pool.lpCoinType, 100_000_000n);
+
+    const minAmount = await CLAMM.quoteRemoveLiquidityOneCoin({
+      pool,
+      coinOutType: pool.coinTypes[0],
+      amount: 100_000_000n,
+    });
+
+    const { txb, coinOut } = await CLAMM.removeLiquidityOneCoin({
+      txb: initTxb,
+      pool,
+      coinOutType: pool.coinTypes[0],
+      lpCoin,
+    });
+
+    txb.transferObjects([coinOut], txb.pure(keypair.toSuiAddress()));
+
+    const response = await executeTx(txb);
+    log(minAmount);
+    log(response);
+  } catch (e) {
+    console.log(e);
+  }
+})();
